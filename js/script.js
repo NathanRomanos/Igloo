b5416d60ffa63146bee0cbc1f276915cbddc3e84
Nathan michael glenn Romanos <932400754@bun2-206-07.local>
2019-11-28 15:30:36 +1300

    Started work on searchbox

--- js/script.js
+++ js/script.js
@@ -1,1 +1,68 @@
 console.log('Working');
+
+
+
+$("#startDate").datepicker({
+  dateFormat: 'yy-mm-dd',
+  changeMonth: true,
+  minDate: new Date(),
+  maxDate: '+1y',
+  onSelect: function(date){
+
+      var selectedDate = new Date(date);
+      var msecsInADay = 86400000;
+      var stDate = new Date(selectedDate.getTime() + msecsInADay);
+
+     //Set Minimum Date of EndDatePicker After Selected Date of StartDatePicker
+      $("#endDate").datepicker( "option", "minDate", stDate );
+      var enDate = new Date(selectedDate.getTime() + 15 * msecsInADay);
+
+
+      $("#endDate").datepicker( "option", "maxDate", enDate );
+
+  }
+});
+
+
+$("#endDate").datepicker({
+  dateFormat: 'yy-mm-dd',
+  changeMonth: true,
+  onSelect: function dateDiff() {
+
+    var start = $('#startDate').datepicker('getDate');
+    var end = $('#endDate').datepicker('getDate');
+    var days   = (end - start)/1000/60/60/24;
+    console.log(days);
+    return;
+  }
+
+});
+
+var number = 0;
+
+document.getElementById('adults').innerText = number;
+
+$("#adultPlus").click(function(){
+  if (number < 6) {
+    number = number+1
+  };
+  document.getElementById('adults').innerText = number;
+});
+
+$("#adultMinus").click(function(){
+  if (number > 0) {
+    number = number-1
+  };
+  document.getElementById('adults').innerText = number;
+});
+
+
+//Find the number of days between dates
+// function dateDiff() {
+//
+//   var start = $('#startDate').datepicker('getDate');
+//   var end = $('#endDate').datepicker('getDate');
+//   var days   = (end - start)/1000/60/60/24;
+//   console.log(days);
+//   return;
+// }
